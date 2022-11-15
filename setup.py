935f0c90952955130479bb55e17d46bb2c97ca68
Rishabh Singh <rishabhsingh971@gmail.com>
2022-11-16 00:00:49 +0530

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='PyUtils',
-    version='0.0.1',
-    author='Rishabh Singh',
-    author_email='rishabhsingh971@gmail.com',
-    description='Testing installation of Package',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/rishabhsingh971/pyutils',
-    project_urls = {
-        "Bug Tracker": "https://github.com/rishabhsingh971/pyutils/issues"
-    },
-    license='MIT',
-    packages=['pyutils'],
-    install_requires=['requests'],
-)
+if __name__ == "__main__":
+    setuptools.setup()
